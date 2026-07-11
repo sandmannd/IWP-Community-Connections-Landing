f489f4414cdc681edc4ad182a7b4b1ed66381a57
sandmannd <sandmannd@gmail.com>
2026-07-11 02:30:06 -0500

    Replace placeholder collage with production landing page hero

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.IWP_SITE_CONFIG = {
   siteName: "IWP Community Connections",
   organizationName: "Invisible Wounds Project",
   supportEmail: "sandmannd@gmail.com",
-  appUrl: "https://script.google.com/macros/s/AKfycbwfxk0IhXkjNih2_IK6zEfJWZp3NxNAImZa8Rr2COGGLb5UNc5Nd7E0yJBGnU82RWGDRA/exec",
+  appUrl: "https://script.google.com/macros/s/AKfycbxPNqswpRLvj0LZ0ZaVw6DTSpTcZIDt0S0zWFJgOES7g864n734dDF2UpGMwdpsEPGK4Q/exec",
   facebookUrl: "https://www.facebook.com/groups/1000568346273790/?sorting_setting=CHRONOLOGICAL",
   iwpWebsiteUrl: "https://invisiblewoundsproject.org/",
   helpPhoneDisplay: "855-HELP-IWP (435-7497)",
